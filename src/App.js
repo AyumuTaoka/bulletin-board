07ead0c387ec6455735f8c7c87576d604e1dae2b
AyumuTaoka <a.taoka@4nonome.com>
2023-10-07 02:30:00 +0900

    リセットCSSの追加

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
-function App() {}
+import { Global, css } from "@emotion/react";
+import normalize from "normalize.css";
+
+import Header from "./organisms/Header";
+
+const styles = css`
+  ${normalize}
+`;
+
+function App() {
+  return (
+    <>
+      <Global styles={styles} />
+      <Header />
+    </>
+  );
+}
 
 export default App;
